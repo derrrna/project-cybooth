97fa81aca8af9c2ff98bba32bcb757ff8af7b829
dpab0001 <dannapabayo@gmail.com>
2025-03-30 20:46:51 +1100

    fixed blob scaling issues

--- cybooth/src/pages/Photobooth.tsx
+++ cybooth/src/pages/Photobooth.tsx
@@ -1,4 +1,4 @@
-import BackgroundFlower from "../components/BackgroundFlower.tsx";
+
 
 export default function Photobooth() {
     return (
@@ -18,25 +18,14 @@ export default function Photobooth() {
             <div className="w-screen h-screen absolute z-[-39] overflow-hidden flex">
                 <div className="w-1/2 h-full relative">
                     <img src="/blob1.png"
-                         className="scale-180 md:w-[40vw] lg:w-[35vw] xl:max-w-[35vw] translate-y-1/4 -translate-x-1/5 absolute bottom-0 left-0"/>
+                         className="scale-180 lg:scale-70 translate-y-1/3 -translate-x-2/7 absolute bottom-0 left-0"/>
                 </div>
                 <div className="w-1/2 h-full relative">
                     <img src="/blob1.png"
-                         className="scale-130 translate-x-1/5  md:w-[40vw] lg:w-[30vw] xl:max-w-[30vw] rotate-180 absolute -top-[6vh] right-0"/>
+                         className="scale-130 translate-x-1/5 -translate-y-1/3 lg:scale-80 rotate-180 absolute right-0"/>
                 </div>
             </div>
 
-            {/* flower layer background 1 - visible only on laptop*/}
-            <div className="hidden absolute z-[-38] w-screen h-screen lg:flex flex-col overflow-hidden">
-                <BackgroundFlower imageSource="/Flower2.png" flowerStyle="-rotate-90 -translate-x-2/5 -translate-y-1/3 scale-[90%]"/>
-                <BackgroundFlower imageSource="" flowerStyle="-rotate-90"/>
-            </div>
-
-            {/* flpwer layer background 2 - visible for mobile, ipads, laptop*/}
-            <div className="absolute z-[-38] w-screen h-screen flex flex-col overflow-hidden">
-                <BackgroundFlower imageSource="/Flower1.png" flowerStyle="rotate-165 scale-80 -translate-y-2/5 lg:scale-100 lg:translate-x-2/7 lg:-translate-y-1/3"/>
-                <BackgroundFlower imageSource="/Flower1.png" flowerStyle="rotate-65  scale-60 -translate-x-3/8 translate-y-2/5 lg:-translate-x-2/7 lg:translate-y-1/3"/>
-            </div>
 
 
             {/* content layer*/}

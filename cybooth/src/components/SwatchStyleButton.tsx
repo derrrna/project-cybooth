c5bf0740b13dc4d3952fbea241edd56294f4039f
dpab0001 <dannapabayo@gmail.com>
2025-11-25 17:11:33 +1100

    Photoeditor page: Added themed photostrip images.

--- cybooth/src/components/SwatchStyleButton.tsx
+++ cybooth/src/components/SwatchStyleButton.tsx
@@ -1,9 +1,13 @@
 import {motion} from "motion/react";
 
-export default function SwatchStyleButton() {
+interface swatchStyleButtonProps {
+    color: string;
+}
+
+export default function SwatchStyleButton({color}: swatchStyleButtonProps) {
     return (
         <motion.button
-            className={'border-4 border-white p-4 rounded-xl w-12 h-12  z-[25] bg-[#CDB4DB] shadow-md cursor-pointer'}
+            className={`border-4 border-white p-4 rounded-xl w-12 h-12  z-[25] shadow-md cursor-pointer bg-[#${color}]`}
             whileHover={{borderColor: "#EBE8E2", scale:1.15}}>
         </motion.button>
     )
